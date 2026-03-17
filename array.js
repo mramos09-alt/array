0ddcf8ace2e377d43d83cf57b060efd97eda1a80
mramos09-alt <m.ramos09@escola.pr.gov.br>
2026-03-17 09:06:37 -0300

    Add files via upload

---
+++ array.js
@@ -0,0 +1,8 @@
+let nomes =["carlos","adriana","ronaldo","raimundo"];
+//            0          1          2          3 
+console.log(numeros[1]);
+
+let numeros = [1,2,3,4];
+//             0 1 2 3
+
+
